4c85edd8a92f5d42b146454124045de842271462
rachael-ojo <adeolaatilade@gmail.com>
2024-02-02 23:52:11 -0800

    Task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+def check_number(number):
+    if number > 0:
+        print("Positive")
+    elif number < 0:
+        print("Negative")
+    else:
+        print("Zero")
+~                     
