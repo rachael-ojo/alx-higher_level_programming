b34ec1a5bdfe97f938f677e3dbada62a448fddcd
rachael-ojo <adeolaatilade@gmail.com>
2024-02-03 02:01:03 -0800

    Task 0

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    a = 10
+    b = 5
+
+    if __name__ == "__main__":
+        from calculator_1 import calculator
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))"""
+
+def sub(a, b):
+    """My subtraction function
+
+    a = 10
+    b = 5
+
+    if __name__ == "__main__"
+        from calculator_1 import calculator
+
+    result = sub(a, b)
+    print("{} - {} = {}".format(a, b, result))"""
+
+def mul(a, b):
+    """My multiplication function
+
+    a = 10
+    b = 5
+
+    if __name__ == "__main__"
+        from calculator_1 import calculator
+
+    result = mul(a, b)
+    print("{} * {} = {}".format(a, b, result))"""
+
+def div(a, b):
+    """My division function
+
+    a = 10
+    b = 5
+
+    if __name__ == "__main__"
+        from calculator_1 import calculator
+
+    result = div(a, b)
+    print("{} / {} = {}".format(a, b, result))"""
