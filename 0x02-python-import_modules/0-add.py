7766efc284044fe1acf02072c9debb853542a715
rachael-ojo <adeolaatilade@gmail.com>
2024-02-03 01:02:27 -0800

    Task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    a = 1
+    b = 2
+
+    if __name__ == "__main__":
+        from add_0 import add
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
