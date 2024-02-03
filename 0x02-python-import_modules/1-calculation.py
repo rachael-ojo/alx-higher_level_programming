80c3a9a1229897b25f1ccf67f6d4bcc0011b03f1
rachael-ojo <adeolaatilade@gmail.com>
2024-02-03 03:54:14 -0800

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 def add(a, b):
-    result = add(a, b)
-def sub(a, b):
-    result = sub(a, b)
-def mul(a, b):
-    result = mul(a, b)
-def div(a, b):
-    result = div(a, b)
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
 if __name__ == "__main__":
-    from calculator_1 import calculation
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, result))
-    print("{} - {} = {}".format(a, b, result))
-    print("{} * {} = {}".format(a, b, result))
-    print("{} / {} = {}".format(a, b, result))
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiply(a, b))
+    print("Division:", divide(a, b))
