a94b11b8d9f7f7e206df1c102a60d5001ef288a6
rachael-ojo <adeolaatilade@gmail.com>
2024-02-07 13:01:30 -0800

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Retrieves the list of available attributes and methods of an object.
+
+    Parameters:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: The list of available attributes and methods of the object.
+
+    Examples:
+        >>> lookup([])
+        ['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+        >>> lookup({})
+        ['__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values']
+    """
+    return [attr for attr in dir(obj) if not attr.startswith("__")]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
