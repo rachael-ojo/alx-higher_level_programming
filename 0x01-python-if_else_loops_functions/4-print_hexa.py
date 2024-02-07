9d47a1632117fb52c88517f7b0baaf3143017975
rachael-ojo <adeolaatilade@gmail.com>
2024-02-07 14:51:43 -0800

    Task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Print numbers from 0 to 98 in decimal and hexadecimal using format string
 for i in range(99):
-    print('{} {}'.format(i, hex(i)), end=' ')
+    print('{} {}'.format(i, hex(i)))
