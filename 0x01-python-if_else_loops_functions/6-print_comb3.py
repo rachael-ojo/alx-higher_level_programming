83f254c84bb9e60b559350f38d0398e081003b65
rachael-ojo <adeolaatilade@gmail.com>
2024-02-08 05:07:51 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Print all possible combinations of two digits
-for i in range(10):
-    for j in range(i + 1, 10):
-        print('{:02d}, '.format(i * 10 + j), end='')
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        print('{:02d}, '.format(1 * 10 + 2), end='')
 
 print()  # Print a newline after the loop
