5c9750e0b679a7147959f3fab071f341d1ba1253
rachael-ojo <adeolaatilade@gmail.com>
2024-02-21 04:07:32 -0800

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its contents to stdout.
+
+    Args:
+        filename (str, optional): The name of the file to be read. Defaults to "".
+
+    Returns:
+        None
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print("An error occurred:", e)
