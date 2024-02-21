012e18535536b159e97858bc5e6472f5bb4e47d9
rachael-ojo <adeolaatilade@gmail.com>
2024-02-21 04:20:07 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -14,3 +14,7 @@ def read_file(filename=""):
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end='')
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print("An error occurred:", e)
