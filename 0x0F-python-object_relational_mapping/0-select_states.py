fc7f55d863f503ef8670478cdcb534b40f8b3148
rachael-ojo <adeolaatilade@gmail.com>
2024-04-15 09:53:35 -0700

    Updated

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    try:
+        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database);
+        cursor = db.cursor()
+
+        sql = "SELECT * FROM states ORDER BY id ASC"
+        cursor.execute(sql)
+        states = cursor.fetchall()
+
+        for state in states:
+            print(f"({state[0]}, '{state[1]}')")
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <root> <@Adeola1234567> <hbtn_0e_0_usa>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, database)
